4176e0651314ee3438598546de6d6038bed4afbd
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2022-10-21 16:42:17 +0300

    Find the Kth Largest Integer in the Array

---
+++ Find the Kth Largest Integer in the Array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums = [int(i) for i in nums]
+        nums = sorted(nums, reverse = True)
+        return str(nums[k - 1])
