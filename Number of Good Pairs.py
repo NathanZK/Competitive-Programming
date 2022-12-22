fc7d291682466ec4831116445f25d0d1f5c19d9b
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2022-12-22 21:47:36 +0300

    Update Number of Good Pairs.py

--- Number of Good Pairs.py
+++ Number of Good Pairs.py
@@ -3,14 +3,12 @@ class Solution:
         count = {}
         for num in nums:
             count[num] = count.get(num, 0) + 1
+
         res = 0
         for n in count.values():
-            res += self.helper(n)
-        
+            if n != 1:
+                res += (n*(n-1))//2
         return res
              
-    def helper(self, n):
-        sum = 0
-        for i in range(1, n):
-            sum += i
-        return sum
+    
+        
