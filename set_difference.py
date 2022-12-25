f96983cd85dfdf3188c100f1c9751e86eae6d1b8
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2022-12-25 15:19:23 +0300

    Create set_difference.py

---
+++ set_difference.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num1 = int(input())
+english = set(map(int, input().split()))
+num2 = int(input())
+french = set(map(int, input(). split()))
+
+print(len(english.difference(french)))
