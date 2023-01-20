77344a66f628bda088c4feb4e6cf74eab27d2909
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-01-20 19:51:13 +0300

    Update Selection Sort.py

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,29 +1,12 @@
 #User function Template for python3
 
 class Solution: 
-    def select(self, arr, i):
-        min_val = min(arr[i:])
-        return min_val
-    
     def selectionSort(self, arr,n):
-        for i in range(len(arr)):
-            index = arr.index(self.select(arr,i), i)
-            arr[i], arr[index] = arr[index], arr[i]
-            
-            
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__': 
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        arr = list(map(int, input().strip().split()))
-        Solution().selectionSort(arr, n)
         for i in range(n):
-            print(arr[i],end=" ")
-        print()
-# } Driver Code Ends
+            minInd = i
+            for j in range(i, n):
+                if arr[j] < arr[minInd]:
+                    minInd = j
+                    
+            arr[minInd], arr[i] = arr[i], arr[minInd]
+            
