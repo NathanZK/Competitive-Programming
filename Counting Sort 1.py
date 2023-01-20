04064b4bec7452e57529ffbb7b5eeca95e19a45b
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-01-20 19:43:51 +0300

    Update Counting Sort 1.py

--- Counting Sort 1.py
+++ Counting Sort 1.py
@@ -6,12 +6,19 @@ import random
 import re
 import sys
 
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
 
 def countingSort(arr):
-    result = [0] * 100
+    countSorted = [0] * 100
     for num in arr:
-        result[num] += 1
-    return result
+        countSorted[num] += 1
+        
+    return countSorted
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
