b0243f2877710f73774fe0e2fe6598e84abfeec9
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-01-22 13:09:08 +0300

    Update Largest Number.py

--- Largest Number.py
+++ Largest Number.py
@@ -1,10 +1,14 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        n = len(nums)
+        size = len(nums)
         nums = [str(i) for i in nums]
-        for i in range(n - 1):
-            for j in range(n - 1 - i):
-                if nums[j] + nums[j + 1] < nums[j + 1] + nums[j]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        for i in range(1, size):
+            key = nums[i]
+            p = i - 1
+            while p >= 0 and (key + nums[p] > nums[p] + key):
+                nums[p+1] = nums[p]
+                p -= 1
+            nums[p+1] = key
 
         return str(int("".join(nums)))
+
