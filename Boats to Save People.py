f00858561ad6fc9fb1ee60b30d5e6c2324096971
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-01-26 18:25:46 +0300

    Update Boats to Save People.py

--- Boats to Save People.py
+++ Boats to Save People.py
@@ -1,14 +1,12 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people.sort(reverse = True)
+        people.sort()
         left, right = 0, len(people) - 1
         boats = 0
         while left <= right:
             if people[left] + people[right] <= limit:
-                boats += 1
                 left += 1
-                right -= 1
-            else:
-                boats += 1
-                left += 1
+            boats += 1
+            right -= 1
+                
         return boats
