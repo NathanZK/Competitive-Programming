9f388cd91e478b561a054cdb4b2c26fe2e2cc714
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-01-26 21:34:43 +0300

    Update Merge Sorted Array.py

--- Merge Sorted Array.py
+++ Merge Sorted Array.py
@@ -4,20 +4,12 @@ class Solution:
         N = n - 1
         right = m + n - 1
         
-        while N >= 0 and right >= 0:
-            if M >= 0:
-                if nums2[N] >= nums1[M]:
-                    nums1[right] = nums2[N]
-                    N -= 1
-                    right -= 1
-                else:
-                    nums1[right] = nums1[M]
-                    M -= 1
-                    right -= 1
+        while N >= 0:
+            if M >= 0 and nums1[M] >= nums2[N]:
+                nums1[right] = nums1[M]
+                M -= 1
             else:
                 nums1[right] = nums2[N]
-                right -= 1
                 N -= 1
-            
-     
+            right -= 1      
         
