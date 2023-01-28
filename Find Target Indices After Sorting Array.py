c748a84fae514ea5d724633a9a5f823b3d852111
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-01-28 20:52:29 +0300

    Update Find Target Indices After Sorting Array.py

--- Find Target Indices After Sorting Array.py
+++ Find Target Indices After Sorting Array.py
@@ -1,8 +1,9 @@
-class Solution(object):
-    def targetIndices(self, nums, target):
-        nums = sorted(nums)
-        result = []
-        for i in range(len(nums)):
-            if nums[i] == target:
-                result.append(i)
-        return result
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        targetIndices = []
+        nums.sort()
+        for index, num in enumerate(nums):
+            if num == target:
+                targetIndices.append(index)
+
+        return targetIndices
