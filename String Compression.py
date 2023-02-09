c8fd2365e69f434badb6b86fe51e0a8f1bfcff46
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-09 22:10:27 +0300

    Update String Compression.py

--- String Compression.py
+++ String Compression.py
@@ -1,15 +1,19 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        left, next = 0, 1
-        while next < len(chars):
-            count = 1
-            while next < len(chars) and chars[left] == chars[next]:
-                count += 1
-                next += 1
-            if count != 1: 
-                chars[left + 1: next] = str(count)
-                left += len(str(count)) + 1
-                next = left + 1
-            else:
-                left += 1
-                next = left + 1
+        left, right = 0, 1
+        while right < len(chars):
+            while right < len(chars) and chars[left] == chars[right]:
+                right += 1
+
+            diff = right - left
+            if diff > 1: 
+                chars[left + 1: right] = str(diff)
+                left += len(str(diff))
+
+            left += 1
+            right = left + 1
+
+        return(len(chars))
+
+  
+   
