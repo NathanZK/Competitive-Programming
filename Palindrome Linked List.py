7eb04ece31e438e08cca209c3a49db0db46d5f7f
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-23 09:29:43 +0300

    Update Palindrome Linked List.py

--- Palindrome Linked List.py
+++ Palindrome Linked List.py
@@ -1,14 +1,20 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def isPalindrome(self, head):
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
         stack = []
         while head:
-            value = head.val
-            stack.append(value)
+            stack.append(head.val)
             head = head.next
-            
-        return stack == stack[::-1]
+
+        left, right = 0, len(stack) - 1
+        while left < right:
+            if stack[left] != stack[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+        
