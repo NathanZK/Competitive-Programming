c208c12b9eda0d68b2af80f601a446e3104aa7e7
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-24 17:51:05 +0300

    Update Evaluate Reverse Polish Notation.py

--- Evaluate Reverse Polish Notation.py
+++ Evaluate Reverse Polish Notation.py
@@ -1,20 +1,24 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        operations = "+-*/"
-        for n in tokens:
-            if n in operations:
-                num2 = int(stack.pop())
-                num1 = int(stack.pop())
-                if n == "+":
-                    res = num1 + num2
-                if n == "-":
-                    res = num1 - num2
-                if n == "*":
-                    res = num1 * num2
-                if n == "/":
-                    res = int(num1 / num2)
-                stack.append(res)
+        for t in tokens:
+            if t == "*":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(val1*val2)
+            elif t == "/":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(int(val2/val1))
+            elif t == "+":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(val2+val1)
+            elif t == "-":
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(val2-val1)
             else:
-                stack.append(n)
+                stack.append(int(t))
+
         return stack[0]
