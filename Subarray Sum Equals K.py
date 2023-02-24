9ce8f1fcf293fbef08cac5aac0951b24c7de5c06
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-24 17:02:05 +0300

    Update Subarray Sum Equals K.py

--- Subarray Sum Equals K.py
+++ Subarray Sum Equals K.py
@@ -1,13 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        sum = 0
+        currSum = 0
         prefixSum = {0 : 1}
-        res = 0
+        sumK = 0
         for i in nums:
-            sum += i
-            diff = sum - k
+            currSum += i
+            diff = currSum - k
             
-            res += prefixSum.get(diff, 0)
-            prefixSum[sum] = 1 + prefixSum.get(sum, 0)
+            sumK += prefixSum.get(diff, 0)
+            prefixSum[currSum] = 1 + prefixSum.get(currSum, 0)
+
+        return sumK
             
-        return res
+            
+        
