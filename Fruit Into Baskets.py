845534dd81c1a57e9a4a814d419062466c41e062
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-27 11:17:49 +0300

    Update Fruit Into Baskets.py

--- Fruit Into Baskets.py
+++ Fruit Into Baskets.py
@@ -1,17 +1,18 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        n = len(fruits)
-        if n == 1: return 1
-        start = 0
-        end = 0
-        mx = 0
-        hashmap = {}
-        while end < n:
-            hashmap[fruits[end]] = end
-            if len(hashmap) > 2:
-                min_val = min(hashmap.values())
-                del hashmap[fruits[min_val]]
-                start = min_val + 1
-            mx = max(mx, end - start + 1)
-            end += 1
-        return mx
+        left, maxFruits = 0, 0
+        fruitMap = {}
+
+        for right in range(len(fruits)):
+            fruitMap[fruits[right]] = fruitMap.get(fruits[right], 0) + 1
+            while len(fruitMap) > 2:
+                fruitMap[fruits[left]] -= 1
+                if fruitMap[fruits[left]] == 0:
+                    del fruitMap[fruits[left]]
+                left += 1
+     
+            maxFruits = max(maxFruits, right - left + 1)
+
+        return maxFruits
+                
+        
