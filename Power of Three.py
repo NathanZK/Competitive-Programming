0afa95f2795e408ff891bdbcedce1fe24d3a2e2a
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-28 14:52:21 +0300

    Update Power of Three.py

--- Power of Three.py
+++ Power of Three.py
@@ -1,5 +1,8 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n == 0: return False
-        if n == 1: return True
-        return n % 3 == 0 and self.isPowerOfThree(n/3)
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+
+        return n % 3 == 0 and self.isPowerOfThree(n//3)
