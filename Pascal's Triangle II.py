1965e915b492a0eed88a9e0f4775b19bc91ef9a3
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-28 14:50:03 +0300

    Update Pascal's Triangle II.py

--- Pascal's Triangle II.py
+++ Pascal's Triangle II.py
@@ -1,1 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+            
+        arr = self.getRow(rowIndex-1)
+        currArr = [1] * (rowIndex+1)
 
+        for i in range(1, rowIndex):
+            currArr[i] = arr[i-1] + arr[i]
+
+        return currArr
