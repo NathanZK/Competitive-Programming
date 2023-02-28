2897741fc3306cac22983a5705c137adc0f9ac0f
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-28 15:09:40 +0300

    Update Power of Four.py

--- Power of Four.py
+++ Power of Four.py
@@ -1,5 +1,8 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n == 0: return False
-        if n == 1: return True
-        return n % 4 == 0 and self.isPowerOfFour(n/4)
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+
+        return n % 4 == 0 and self.isPowerOfFour(n//4)
