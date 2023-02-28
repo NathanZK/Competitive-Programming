57486862e4e01f46e72ac64a42fa5a954f2107e9
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-28 22:53:44 +0300

    Update Car Pooling.py

--- Car Pooling.py
+++ Car Pooling.py
@@ -1,25 +1,15 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips = sorted(trips, key = lambda x: x[1])
-        numPass = trips[0][0]
-        if numPass > capacity:
-            return False
-        trP = trips.copy()
-        for n in range(1, len(trips)):
-            numPass += trips[n][0]
-            start = trips[n][1]
-            i = 0
-            m = n
-            while i < m:
-                try:
-                    if start >= trP[i][2]:
-                        numPass -= trP[i][0]
-                        trP.pop(i)
-                        m -= 1
-                        continue
-                except:
-                    pass
-                i += 1
-            if numPass > capacity:
+        prefix = [0] * 1002
+        for num, start, end in trips:
+            prefix[start] += num
+            prefix[end] -= num
+
+        runningSum = 0
+        for i in range(len(prefix)-1):
+            runningSum += prefix[i]
+            if runningSum > capacity:
                 return False
+
         return True
+        
