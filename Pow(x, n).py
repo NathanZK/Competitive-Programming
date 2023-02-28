c30dbf5540dcba5186799019e6b89c68d86cd94a
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-28 16:34:52 +0300

    Update Pow(x, n).py

--- Pow(x, n).py
+++ Pow(x, n).py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def myPow(self, x, n):
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
         if n < 0:
-            return 1 / self.myPow(x, abs(n))     
-        elif n == 0:
-            return 1.0
-        elif n % 2 == 1:
-            return self.myPow(x, n - 1) * x
-        elif n % 2 == 0:
-            return self.myPow(x * x, n // 2)
+            return 1 / (self.myPow(x, abs(n)))
+        if n % 2 == 1:
+            return x * self.myPow(x, n - 1)
+
+        root = self.myPow(x, n // 2)
+        return root * root
