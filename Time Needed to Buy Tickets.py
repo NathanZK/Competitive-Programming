86f9b21b2ec795caab10e7266dc6e95a00240ca2
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-28 15:17:21 +0300

    Create Time Needed to Buy Tickets.py

---
+++ Time Needed to Buy Tickets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+
+        while True:
+            for t in range(len(tickets)):
+                if tickets[t] != 0:
+                    time += 1
+                    tickets[t] -= 1
+
+                if t == k and tickets[t] == 0:
+                    return time
