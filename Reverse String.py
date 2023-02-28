49f0a4ad7120fb763168760a779a64d2dafd19d3
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-02-28 15:58:18 +0300

    Update Reverse String.py

--- Reverse String.py
+++ Reverse String.py
@@ -1,9 +1,11 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        left, right = 0, len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+        self.revString(s, 0, len(s) - 1)
 
+    def revString(self, s, left, right):
+        if left >= right:
+            return s
+
+        s[left], s[right] = s[right], s[left]    
+        self.revString(s, left+1, right - 1)
         
