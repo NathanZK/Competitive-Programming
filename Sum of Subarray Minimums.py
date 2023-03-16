f7e70f6d3235b4784cfe6d71534c3d7ba3194681
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-03-16 14:12:40 +0300

    Update Sum of Subarray Minimums.py

--- Sum of Subarray Minimums.py
+++ Sum of Subarray Minimums.py
@@ -1,14 +1,14 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        decStack = [-1]
+        incStack = [-1]
         arr.append(0)
         minSum = 0
 
         for i, num in enumerate(arr):
-            while len(decStack) > 1 and num < arr[decStack[-1]]:
-                ind = decStack.pop()
-                minSum += (arr[ind]*(i-ind)*(ind-decStack[-1]))
+            while len(incStack) > 1 and num < arr[incStack[-1]]:
+                ind = incStack.pop()
+                minSum += (arr[ind]*(i-ind)*(ind-incStack[-1]))
 
-            decStack.append(i)
+            incStack.append(i)
 
         return minSum % (10**9 + 7)
