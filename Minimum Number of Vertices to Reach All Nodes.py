816aa422a7e23b654976062dfbfcb5c252d57b81
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-04-06 18:50:36 +0300

    Update Minimum Number of Vertices to Reach All Nodes.py

--- Minimum Number of Vertices to Reach All Nodes.py
+++ Minimum Number of Vertices to Reach All Nodes.py
@@ -1,18 +1,12 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-
+        graph = [0 for _ in range(n)]
         for src, des in edges:
-            graph[src].append(des)
+            graph[des] = 1
+        
+        smallest = []
+        for i in range(len(graph)):
+            if graph[i] == 0:
+                smallest.append(i)
 
-        for key, val in graph.items():
-            for v in val:
-                if v != -1 and v in graph:
-                    graph[v].append(-1)
-
-        smallestVertices = []
-        for key, val in graph.items():
-            if val[-1] != -1:
-                smallestVertices.append(key)
-
-        return smallestVertices
+        return smallest
