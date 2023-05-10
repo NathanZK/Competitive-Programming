f4950970b76c05dffb032df325182fdac1e259b0
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-05-10 17:35:25 +0300

    added course-schedule.python

---
+++ course-schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        incoming = Counter()
+        queue = deque()
+        order = []
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            incoming[course] += 1
+
+        for course in range(numCourses):
+            if incoming[course] == 0:
+                queue.append(course)
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+
+            for neighbor in graph[course]:
+                incoming[neighbor] -= 1
+                if incoming[neighbor] == 0:
+                    queue.append(neighbor)
+
+        if len(order) != numCourses:
+            return False
+
+        return True
