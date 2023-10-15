89e66240fb00f50a48180b01a8f385b7002309c1
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-10-15 22:32:09 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,9 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        path = [[1 for _ in range(n)] for _ in range(m)]
+        squares = (m+n-2)
+        n -= 1
+        m -= 1
+        paths = 1
 
-        for i in range(1, len(path)):
-            for j in range(1, len(path[0])):
-                path[i][j] = path[i-1][j] + path[i][j-1]
+        while squares > max(m, n):
+            paths *= squares
+            squares -= 1
 
-        return path[-1][-1]
+        _min = min(n, m)
+        while _min > 1:
+            paths /= _min
+            _min -= 1
+
+        return int(paths)
