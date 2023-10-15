d2471ccfbec30dd89591fe30a701004b52f5367c
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-10-15 21:16:23 +0300

    added count-unhappy-friends.python

---
+++ count-unhappy-friends.py
@@ -0,0 +1,25 @@
+class Solution:
+    def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
+        hashPairs = {}
+        for num1, num2 in pairs:
+            hashPairs[num1] = num2
+            hashPairs[num2] = num1
+        
+        unhappy = set()
+        for i in range(n):
+            pair1 = hashPairs[i]
+
+            for p1 in preferences[i]:
+                if p1 == pair1:
+                    break
+
+                pair2 = hashPairs[p1]
+                for p2 in preferences[p1]:
+                    if p2 == pair2:
+                        break
+
+                    if p2 == i:
+                        unhappy.add(i)
+                        unhappy.add(p1)
+
+        return len(unhappy)
