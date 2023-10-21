3b4e049358430ac1bed37bf6ea3cafb2ccebaa56
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-10-21 19:23:14 +0300

    added basic-calculator-ii.python

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -4,38 +4,32 @@ class Solution:
         stack = []
 
         for n in nums:
-            if n.isdigit():
-                if stack and (stack[-1] == "*" or stack[-1] == "/"):
-                    op = stack.pop()
-                    num = stack.pop()
+            if stack and (stack[-1] == "*" or stack[-1] == "/"):
+                op = stack.pop()
+                num = int(stack.pop())
 
-                    if op == "*":
-                        stack.append(int(n)*num)
-                    else:
-                        stack.append(num//int(n))
+                if op == "*":
+                    stack.append(int(n)*num)
                 else:
-                    stack.append(int(n))
+                    stack.append(num//int(n))
             else:
                 stack.append(n)
 
         nums = stack
         stack = []
         for n in nums:
-            if str(n).isdigit():
-                if stack and (stack[-1] == "+" or stack[-1] == "-"):
-                    op = stack.pop()
-                    num = stack.pop()
+            if stack and (stack[-1] == "+" or stack[-1] == "-"):
+                op = stack.pop()
+                num = int(stack.pop())
 
-                    if op == "+":
-                        stack.append(n+num)
-                    else:
-                        stack.append(num-n)
+                if op == "+":
+                    stack.append(int(n)+num)
                 else:
-                    stack.append(n)
+                    stack.append(num-int(n))
             else:
                 stack.append(n) 
 
-        return stack[0]
+        return int(stack[0])
  
 
     def separateNums(self, s):
